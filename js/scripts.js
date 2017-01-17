08a099770fd633607faeb4aba315d0e48e3becb1
Jiwon Kang and keely glenn <keely@>
2017-01-17 14:27:33 -0800

    create a 1 question quiz that will result in 2 different results

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
+
 $(document).ready(function(){
   $("form#future-survey").submit(function(event){
     event.preventDefault();
-    $("#zodiac-response").show();
+    
+    var number = 0;
+
     $("input:checkbox[name=zodiac]:checked").each(function(){
       var zodiac = $(this).val();
       $('#zodiac-response').append(zodiac + "<br>");
+      number++;
     });
 
-    $("#globe-response").show();
-    $("input:checkbox[name=globe]:checked").each(function(){
-      var globe = $(this).val();
-      $('#globe-response').append(globe + "<br>");
-    });
+    var result = function() {
+      if(number <= 2) {
+        return "Your future is great!";
+      } else if (number === 3 || number === 4) {
+        return "Your future sucks!!!";
+      }
+    };
+    $("#zodiac-response").text(result);
+
     $('#future-survey').hide();
   });
 });
