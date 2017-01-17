986ad09ac730c101e799aa8aa3319f22f8afc465
Jiwon Kang and keely glenn <jiwon@>
2017-01-17 14:31:55 -0800

    create a 2 question quiz with multiple answers

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,29 @@
 $(document).ready(function(){
   $("form#future-survey").submit(function(event){
     event.preventDefault();
-    
-    var number = 0;
 
+    var number = 0;
+    var number2 = 0;
     $("input:checkbox[name=zodiac]:checked").each(function(){
       var zodiac = $(this).val();
-      $('#zodiac-response').append(zodiac + "<br>");
       number++;
     });
 
+    $("input:checkbox[name=globe]:checked").each(function(){
+      var globe = $(this).val();
+      number2++;
+    });
+
     var result = function() {
-      if(number <= 2) {
+      if(number <= 2 && number2 <= 2) {
         return "Your future is great!";
-      } else if (number === 3 || number === 4) {
+      } else if (number === 3 || number === 4 && number2 === 3 || number2 === 4) {
         return "Your future sucks!!!";
+      } else {
+        return "you have no future. you will die soon";
       }
     };
-    $("#zodiac-response").text(result);
+    $("#response").text(result);
 
     $('#future-survey').hide();
   });
